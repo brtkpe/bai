31b8e4119900a5ce5fd020c1727357270538b0b9
Bartlomiej Podgorni <bartekpodgorni@gmail.com>
2020-03-25 15:55:32 +0100

    last exc 02

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -1,28 +1,30 @@
 names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
-
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
-    startWith(letter){
-        return // names (array) which starts with the letter
+    startWith(letter) {
+        const result = this.names.filter(name => name[0] === letter);
+
+        return new Students(result);
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return new Students(this.names.sort());
     }
 
     get() {
-        return // names (array) in natural order
+        return this.names;
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        return this.names.slice(0, n);
     }
 }
 
 students = new Students(names);
+
 console.log(students.get());
 console.log(students.sort());
 console.log(students.startWith('P'));
@@ -30,6 +32,3 @@ console.log(students.startWith('M').sort());
 console.log(students.sort().getFirst(4));
 console.log(students.startWith('M').sort().getFirst(2));
 console.log(students.get());
-
-
-
